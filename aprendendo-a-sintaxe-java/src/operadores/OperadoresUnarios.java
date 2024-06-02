60c28816035b8a259366e3529dd087be1f6b9eff
diasdavizin <davizin042005@gmail.com>
2024-06-02 16:47:14 -0300

    feat: add exercises about unary operators

---
+++ aprendendo-a-sintaxe-java/src/operadores/OperadoresUnarios.java
@@ -0,0 +1,49 @@
+package operadores;
+
+public class OperadoresUnarios {
+    public static void main(String [] args ) {
+
+        // Exemples of Operators Unary in Java
+
+
+        // This exemple show how to make one variable be negative with unary.
+
+         int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        // And this exemple show how to make one variable be positive with unary.
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        // This exemple show how to inclemente one number.
+
+        /*int numero = 5;
+
+        numero++;
+
+        System.out.println(numero);
+
+
+        // This exemple show how to declement one number.
+
+        int numero = 5;
+
+        numero--;
+
+        System.out.println(numero);
+
+        // This example shows how to negate a Boolean expression.
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+}
