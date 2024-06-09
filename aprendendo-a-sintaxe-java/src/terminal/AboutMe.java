4435f540d1a896dc73b6eab90c4d92841b21c8fc
diasdavizin <davizin042005@gmail.com>
2024-06-09 16:51:48 -0300

    feat: creating the object scanner to read the user input better

--- aprendendo-a-sintaxe-java/src/terminal/AboutMe.java
+++ aprendendo-a-sintaxe-java/src/terminal/AboutMe.java
@@ -1,17 +1,32 @@
 package terminal;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
 
-        // arguments to be passed in the terminal
+        //Creating the object scanner to read the user input
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]); // Wrappers
-        double altura = Double.valueOf(args[3]);
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
 
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+
+        // Printing the data obtained by the user
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
+
     }
 }
+
