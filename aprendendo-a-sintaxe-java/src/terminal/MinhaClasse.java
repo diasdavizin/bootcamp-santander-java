293ec1abd03550a78d95da70af6adf0e3eb57aa3
diasdavizin <davizin042005@gmail.com>
2024-06-09 16:43:05 -0300

    feat: add exercises testing how to run java program via terminal

---
+++ aprendendo-a-sintaxe-java/src/terminal/MinhaClasse.java
@@ -0,0 +1,10 @@
+package terminal;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+
+        // Test about how to run a Java program via terminal such as MS - DOS or IntelliJ terminal.
+
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
