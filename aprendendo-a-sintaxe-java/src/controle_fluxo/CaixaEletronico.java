e7553164802b54d5b881e6fcb7f3a727b82b7060
diasdavizin <davizin042005@gmail.com>
2024-06-12 20:51:04 -0300

    feat: add exercises about simple conditional

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/CaixaEletronico.java
@@ -0,0 +1,17 @@
+package controle_fluxo;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+
+        // Exemple of Simple Condicional in Java
+
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+
+    }
+}
