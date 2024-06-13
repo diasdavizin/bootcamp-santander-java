18060db50c663a5f77020e272920ea26f468f788
diasdavizin <davizin042005@gmail.com>
2024-06-12 21:21:34 -0300

    feat: add exercise about ternary conditional

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/ResultadoEscolar_2.java
@@ -0,0 +1,12 @@
+package controle_fluxo;
+
+public class ResultadoEscolar_2 {
+    public static void main(String[] args) {
+
+        // Example of Ternary Operator in Java
+
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
