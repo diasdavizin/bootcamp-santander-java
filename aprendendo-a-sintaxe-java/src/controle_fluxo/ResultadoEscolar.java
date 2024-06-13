2d803e2c6ba5a7ebec8c6a0a214c6ce85474cf39
diasdavizin <davizin042005@gmail.com>
2024-06-12 21:02:03 -0300

    feat: add exercise about composite conditionals

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/ResultadoEscolar.java
@@ -0,0 +1,16 @@
+package controle_fluxo;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        // Exemple of composite conditionals in Java
+
+        int nota = 6;
+
+        if(nota >= 7)
+            System.out.println("Aprovado");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
