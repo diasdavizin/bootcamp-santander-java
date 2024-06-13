e0b5a01efe80e332a5e20c6ed2c6729cb67183f5
diasdavizin <davizin042005@gmail.com>
2024-06-12 21:34:19 -0300

    feat: add exercise about Switch Case

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/SistemaMedida.java
@@ -0,0 +1,27 @@
+package controle_fluxo;
+
+public class SistemaMedida {
+    public static void main(String[] args) {
+
+        // Example of Switch Case in Java
+
+        String sigla = "M";
+
+        switch (sigla) {
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("MÉDIO");
+                break;
+            }
+            case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+            default:
+                System.out.println("INDEFINIDO");
+        }
+    }
+}
