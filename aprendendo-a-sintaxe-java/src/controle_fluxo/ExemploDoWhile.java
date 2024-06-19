f569778e1568a0759c74051a31c55606f5045116
diasdavizin <davizin042005@gmail.com>
2024-06-19 19:58:13 -0300

    feat: add exercise about do while loop

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/ExemploDoWhile.java
@@ -0,0 +1,25 @@
+package controle_fluxo;
+
+import java.util.Random;
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+
+        // Example of Do While Loop in Java
+
+        System.out.println("Discando...");
+
+        do {
+
+            System.out.println("Telefone tocando");
+
+        }while(tocando());
+
+        System.out.println("Alô !!!");
+    }
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+
+        return ! atendeu;
+    }
+}
