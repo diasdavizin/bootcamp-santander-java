85bbbf6a73623a2ca43f96edd757767149e6f10d
diasdavizin <davizin042005@gmail.com>
2024-06-19 19:15:58 -0300

    feat: add exercise about for array loop

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/ExemploForArray.java
@@ -0,0 +1,14 @@
+package controle_fluxo;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        // Example of For Array Loop in Java
+
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x = " + x + " é " + alunos [x] );
+            }
+    }
+}
