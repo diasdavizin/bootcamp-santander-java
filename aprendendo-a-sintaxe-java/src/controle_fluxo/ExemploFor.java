918433452385fd64c4ecf77be5fe457bb42c7edd
diasdavizin <davizin042005@gmail.com>
2024-06-19 19:07:07 -0300

    feat: add exercise about for loop

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/ExemploFor.java
@@ -0,0 +1,12 @@
+package controle_fluxo;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        // Example of For Loop in Java
+
+        for ( int lamb = 0; lamb <= 20; lamb ++ ) {
+            System.out.println("Counting sheep: " + lamb);
+        }
+    }
+}
