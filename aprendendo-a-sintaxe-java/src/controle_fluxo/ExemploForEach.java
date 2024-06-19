64adafb9c2752e331feb1966becf9c564e33eeef
diasdavizin <davizin042005@gmail.com>
2024-06-19 19:17:42 -0300

    feat: add exercise about foreach loop

---
+++ aprendendo-a-sintaxe-java/src/controle_fluxo/ExemploForEach.java
@@ -0,0 +1,14 @@
+package controle_fluxo;
+
+public class ExemploForEach {
+    public static void main(String[] args) {
+
+            // Example of ForEach Loop in Java
+
+            String[] alunos = {"Ana", "Bia", "Carlos", "Daniel", "Eduarda"};
+
+            for (String aluno : alunos) {
+                System.out.println("O nome do aluno é: " + aluno);
+            }
+    }
+}
